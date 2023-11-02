ab4f02f8ce896273b3678a40b32f24d0b821905f
Lucas Czulak <lucas.czulak@buddy.works>
2023-11-02 12:24:36 +0100

    Add ScraperPhotoBlog script

---
+++ Readme.md
@@ -0,0 +1,59 @@
+# Web Scraper dla Photoblog.pl
+
+Ten skrypt jest narzędziem do pobierania i zapisywania zdjęć oraz informacji z postów na stronie Photoblog.pl.
+
+## Wymagania
+
+* Python 3.x
+* Pip (zarządca pakietów dla Pythona)
+
+## Instalacja
+
+Kroki instalacyjne dla użytkowników systemów uniksowych i MacOS:
+
+### 1. Sklonuj repozytorium
+
+Za pomocą terminala sklonuj repozytorium do lokalnego katalogu:
+
+git clone https://twoje-repozytorium.git
+cd sciezka-do-twojego-repozytorium
+
+
+### 2. (Opcjonalnie) Utwórz środowisko wirtualne
+
+Aby utworzyć izolowane środowisko dla zależności Pythona, wykonaj:
+
+
+### 3. Zainstaluj zależności
+
+Zainstaluj wymagane pakiety używając `pip`:
+
+pip install -r requirements.txt
+
+
+## Uruchomienie skryptu
+
+Aby uruchomić skrypt, upewnij się, że jesteś w katalogu zawierającym `scraper.py` i wykonaj:
+
+python scraper.py
+
+
+## Struktura katalogów
+
+Po uruchomieniu skryptu zostanie utworzona następująca struktura katalogów:
+
+_AssetsLinks/
+1231231231/
+1231231231.jpg
+1231231231.md
+
+
+## Ważne informacje
+
+* Przed uruchomieniem upewnij się, że masz prawo do pobierania i użytkowania treści z danego serwisu internetowego.
+* Skrypt przeznaczony jest wyłącznie do użytku edukacyjnego.
+
+## Licencja
+
+O ile nie zaznaczono inaczej, kody źródłowe są udostępnione na licencji MIT.
+
---
+++ scraper.py
@@ -0,0 +1,121 @@
+import os
+import time
+import requests
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from bs4 import BeautifulSoup
+from urllib.parse import unquote
+
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+}
+
+# Setup Chrome options
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')  # For headless browser operation
+
+# Initialize Selenium WebDriver
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+def save_image(img_url, save_path):
+    img_name = os.path.basename(unquote(img_url)).split('?')[0]
+    img_folder_path = os.path.join(save_path, os.path.splitext(img_name)[0])
+    os.makedirs(img_folder_path, exist_ok=True)
+    img_path = os.path.join(img_folder_path, img_name)
+
+    if os.path.isfile(img_path):
+        print(f"Plik {img_path} już istnieje, pomijanie pobierania pliku.")
+        return img_name
+
+    response = requests.get(img_url, stream=True, headers=HEADERS)
+    response.raise_for_status()
+
+    with open(img_path, 'wb') as file:
+        for chunk in response.iter_content(chunk_size=8192):
+            file.write(chunk)
+
+    return img_name
+
+def download_post(post_url, save_path):
+    print(f"Pobieranie: {post_url}")
+    response = requests.get(post_url, headers=HEADERS)
+    response.raise_for_status()
+
+    soup = BeautifulSoup(response.content, 'html.parser')
+    img_elem = soup.find('img', id='show_pic_viewer')
+    date_elem = soup.find('span', class_='date')
+    title_elem = soup.select_one('h1.title .inplace_item_width')
+    content_elem = soup.find('div', id='photo_note')
+
+    title_text = title_elem.get_text(strip=True) if title_elem else "Brak tytułu"
+
+    if img_elem:
+        img_url = img_elem['src']
+        img_name = save_image(img_url, save_path)
+        img_md_path = f"/{os.path.join(save_path, img_name.rsplit('.', 1)[0], img_name)}"
+    else:
+        print(f"Nie znaleziono elementu obrazu w poście: {post_url}")
+        return
+
+    txt_file_name = os.path.basename(img_name).split('.')[0] + '.md'
+    txt_file_path = os.path.join(save_path, txt_file_name)
+
+    if os.path.isfile(txt_file_path):
+        print(f"Plik {txt_file_path} już istnieje, pomijanie tworzenia.")
+        return
+
+    with open(txt_file_path, 'w', encoding='utf-8') as file:
+        file.write(f"---\ntagi: DailyNote\n---\n")
+        if title_elem and date_elem:
+            title_text = title_elem.get_text(strip=True)
+            date_text = date_elem.get_text(strip=True).replace('Dodane ', '')
+            file.write(f"### {title_text}\nData: {date_text}\n\n")
+        elif date_elem:
+            title_text = date_elem.get_text(strip=True).replace('Dodane ', '')
+            file.write(f"#### {title_text}\n\n")
+        if post_url:
+            file.write(f"Link do wpisu: [{post_url}]({post_url}) \n\n")
+        if img_elem:
+            file.write(f"![]({img_md_path})\n\n")
+        if content_elem:
+            file.write(f"{content_elem.get_text(strip=True)}\n")
+
+
+
+def get_all_post_links(profile_name):
+    archive_url = f"https://www.photoblog.pl/{profile_name}/archiwum"
+    driver.get(archive_url)
+    last_height = driver.execute_script("return document.body.scrollHeight")
+
+    while True:
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(2)  # Wait for page to load
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+        last_height = new_height
+
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    post_links = [a['href'] for a in soup.select(f'div.arch_post a[href^="https://www.photoblog.pl/{profile_name}/"]')]
+
+    return post_links
+
+def main():
+    # Pobiera nazwę profilu i ścieżkę do zapisu od użytkownika
+    profile_name = input("Podaj nazwę profilu: ").strip()
+    save_path = input("Podaj ścieżkę do zapisu (np. _AssetsLinks): ").strip()
+
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+
+    post_links = get_all_post_links(profile_name)
+
+    print(f"Found {len(post_links)} post links")
+    for link in post_links:
+        download_post(link, save_path)
+
+    driver.quit()
+
+if __name__ == "__main__":
+    main()
